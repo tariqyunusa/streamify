54b7a70ec7744baeb6ccf62ca4732fbc71c8abae
Tariq Yunusa <tariikhyunusa@gmail.com>
2023-12-29 11:28:40 +0100

    50% of Home Section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import Nav from '@/components/Nav'
+
 export const metadata: Metadata = {
   title: 'Streamify',
   description: 'Track Your Listening Habits',
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Nav />
+      
       <body>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import "../styles/page.css"
 import "./globals.css"
-
+import icon from "../public/Spotify_Icon_RGB_White.png"
+import Image from "next/image"
+import Nav from '@/components/Nav'
 
 export default function Home() {
   return (
-    <main>
-      
+    
+    <main className="home_container">
+      <Nav />
+      <section className="body__container">
+      <div className="left__container">
+        <h3 className="left__container_h3">Track Your Listening Habits on <span className="span_container">Spotify</span>.</h3>
+        <button className="container____btn"><Image src={icon} width={25} height={25} alt="spotify icon" /><span className="btn___text">Proceed With Spotify</span></button>
+      </div>
+      </section>
     </main>
   )
 }
