992680f2f18a19ed4948f50df9d86b691b34fa3d
Tariq Yunusa <tariikhyunusa@gmail.com>
2024-03-07 12:11:52 +0100

    potential type fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from 'next'
+import { Work_Sans} from 'next/font/google'
 
 export const metadata: Metadata = {
   title: 'Streamify',
   description: 'Track Your Listening Habits',
 }
+
+const WorkSans = Work_Sans({
+  subsets: ["latin"],
+  display: "swap"
+})
 
 export default function RootLayout({
   children,
@@ -11,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={WorkSans.className}>
       
       <body>{children}</body>
     </html>
